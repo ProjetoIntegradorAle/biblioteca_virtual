001be4326f97ee968a9daf422d4f78b6a518bd1f
Luana Lima <luanatayna2007@gmail.com>
2025-02-12 15:14:03 -0300

    ...

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('meus_materiais/', views.meus_materiais, name="meus_materiais"),
+    path('adicionar_material', views.adicionar_material, name='adicionar_material'),
     path('login/', views.login, name='login'),
     path('editar/<int:id>/', views.editar_material, name='editar_material'),
     path('deletar/<int:id>/', views.deletar_material, name='deletar_material'),
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect,  get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Material
 from .forms import MaterialForm
@@ -6,13 +6,13 @@ from .forms import MaterialForm
 def index(request):
     return render(request, 'index.html')
 
-def login(request):
+def login(request):  # Renomeado para login_view
     return render(request, 'registration/login.html')
 
 @login_required
 def adicionar_material(request):
     if request.method == 'POST':
-        form = MaterialForm(request.POST, request.FILES)
+        form = MaterialForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('meus_materiais')
@@ -36,18 +36,18 @@ def meus_materiais(request):
 def editar_material(request, id_material):
     material = get_object_or_404(Material, pk=id_material)
     if request.method == "POST":
-        form = MaterialForm(request.POST, request.FILES, instance=material)
+        form = MaterialForm(request.POST, instance=material)
         if form.is_valid():
             form.save()
             return redirect('meus_materiais')
     else:
         form = MaterialForm(instance=material)
-    return render(request, 'editar_material.html', {'form': form})
+    return render(request, 'adicionar_material.html', {'form': form})
 
 @login_required
 def deletar_material(request, id_material):
     material = get_object_or_404(Material, pk=id_material)
     if request.method == 'POST':
         material.delete()
-        return redirect('index')
+        return redirect('meus_materiais')
     return render(request, 'deletar_material.html')
