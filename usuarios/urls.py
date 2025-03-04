bd22b2fd89563bc1e78e2db713f42f515978f310
Luana <luanatayna2007@gmail.com>
2025-03-04 15:46:45 -0300

    fazendo templates de recuperar senha

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('resetar-senha/sucesso/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('resetar-senha/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('resetar-senha/completo/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('alterar-senha/', auth_views.PasswordChangeView.as_view(template_name='password_change_form.html'), name='password_change'),
+    path('alterar-senha/sucesso/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
 ]
