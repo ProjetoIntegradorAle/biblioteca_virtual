f590b2e5673e2bd4a5d10b20b852fc267de7525b
luanatslima <luanatayna2007@gmail.com>
2025-04-22 16:13:50 -0300

    criação do models material salvo e da views salvar_material, alterações na views meus_materiais e nos templates busca e meus_materiais

--- app/models.py
+++ app/models.py
@@ -24,3 +24,12 @@ class Material(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+
+class MaterialSalvo(models.Model):
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    material = models.ForeignKey('Material', on_delete=models.CASCADE)
+    data_salvo = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.usuario.username} salvou {self.material.titulo}"
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Material
+from .models import Material, MaterialSalvo
 from .forms import MaterialForm
 from django.core.paginator import Paginator
 from django.db.models import Q
@@ -11,16 +11,27 @@ def index(request):
 
 @login_required
 def meus_materiais(request):
-    form = MaterialForm()
-    materiais = Material.objects.filter(usuario=request.user)  # Filtra materiais pelo usuário logado
-    paginator = Paginator(materiais, 6)
+    form = MaterialForm() 
+    materiais_criados = Material.objects.filter(usuario=request.user) # Materiais criados pelo usuário logado
+    # Materiais salvos pelo usuário logado, separados por tipo
+    materiais_documentos_salvos = MaterialSalvo.objects.filter(usuario=request.user, material__tipo='Documento')
+    materiais_videos_salvos = MaterialSalvo.objects.filter(usuario=request.user, material__tipo='Vídeo')
+    materiais_slides_salvos = MaterialSalvo.objects.filter(usuario=request.user, material__tipo='Slide')
 
-    page_number = request.GET.get('page')
+    # Paginação para materiais criados
+    paginator = Paginator(materiais_criados, 6)
+    page_number = request.GET.get('page')  # Número da página
     page_obj = paginator.get_page(page_number)
+
     return render(request, 'meus_materiais.html', {
         'form': form,
-        'page_obj': page_obj,
+        'page_obj': page_obj,  # Materiais criados paginados
+        'documentos_salvos': materiais_documentos_salvos,
+        'videos_salvos': materiais_videos_salvos,
+        'slides_salvos': materiais_slides_salvos,
     })
+    
+# qual a diferença de pk=id_material e id=material_id?
 
 @login_required
 def adicionar_material(request):
@@ -43,6 +54,7 @@ def adicionar_material(request):
 def visualizar_material(request, id_material):
     material = get_object_or_404(Material, pk=id_material)
     return render(request, 'visualizar_material.html', {'material': material})
+
 
 @login_required
 def editar_material(request, id_material):
@@ -82,3 +94,12 @@ def buscar_materiais(request):
         ) 
 
     return render(request, 'busca.html', {'materiais': materiais})
+
+@login_required
+def salvar_material(request, id_material):
+    if request.method == 'POST':  # Certifique-se de que o salvamento seja feito via POST
+        material = get_object_or_404(Material, id=id_material)  # Verifica se o material já foi salvo
+        material_existente = MaterialSalvo.objects.filter(usuario=request.user, material=material).exists()
+        if not material_existente:
+            MaterialSalvo.objects.create(usuario=request.user, material=material)
+        return redirect('buscar_materiais')  # Redireciona de volta para a página de busca
