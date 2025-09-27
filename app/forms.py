7876a287028e1ab0efee0ca0706ad7e4632d351a
luanatslima <luanatayna2007@gmail.com>
2025-09-27 13:44:30 -0300

    aceitar email de colaborador

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,15 @@ from django import forms
 from .models import Material
 
 class MaterialForm(forms.ModelForm):
+    email_colaborador = forms.EmailField(
+        required=False,
+        label="E-mail do colaborador",
+        widget=forms.EmailInput(attrs={'placeholder': 'Digite o e-mail do colaborador'})
+    )
+
     class Meta:
         model = Material
-        fields = ['titulo', 'descricao', 'tipo', 'arquivo', 'avaliacoes_habilitadas', 'colaboracao_habilitada']
+        fields = ['titulo','descricao','tipo','arquivo','avaliacoes_habilitadas','colaboracao_habilitada']
         labels = {
             'titulo': 'Título',
             'descricao': 'Descrição',
@@ -13,4 +19,3 @@ class MaterialForm(forms.ModelForm):
             'avaliacoes_habilitadas': 'Habilitar Avaliações',
             'colaboracao_habilitada': 'Habilitar Colaboração',
         }
-        
